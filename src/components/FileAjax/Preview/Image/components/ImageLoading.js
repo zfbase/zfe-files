019261cf3e165603921c7ad821ec49361c07cfd0
Ilya Serdyuk <ilya@serdyuk.pro>
2020-07-15 11:21:34 +0300

    Исправил проблему с огромными превьюшками при загрузке картинок

--- src/components/FileAjax/Preview/Image/components/ImageLoading.js
+++ src/components/FileAjax/Preview/Image/components/ImageLoading.js
@@ -7,13 +7,20 @@ import ProgressCircle from '../../../../ProgressCircle/index';
 // eslint-disable-next-line max-len
 const LoadingMock = 'data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTcxIiBoZWlnaHQ9IjE4MCIgdmlld0JveD0iMCAwIDE3MSAxODAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzEwMCV4MTgwCkNyZWF0ZWQgd2l0aCBIb2xkZXIuanMgMi42LjAuCkxlYXJuIG1vcmUgYXQgaHR0cDovL2hvbGRlcmpzLmNvbQooYykgMjAxMi0yMDE1IEl2YW4gTWFsb3BpbnNreSAtIGh0dHA6Ly9pbXNreS5jbwotLT48ZGVmcz48c3R5bGUgdHlwZT0idGV4dC9jc3MiPjwhW0NEQVRBWyNob2xkZXJfMTZmYWRhMDg4OWUgdGV4dCB7IGZpbGw6I0FBQUFBQTtmb250LXdlaWdodDpib2xkO2ZvbnQtZmFtaWx5OkFyaWFsLCBIZWx2ZXRpY2EsIE9wZW4gU2Fucywgc2Fucy1zZXJpZiwgbW9ub3NwYWNlO2ZvbnQtc2l6ZToxMHB0IH0gXV0+PC9zdHlsZT48L2RlZnM+PGcgaWQ9ImhvbGRlcl8xNmZhZGEwODg5ZSI+PHJlY3Qgd2lkdGg9IjE3MSIgaGVpZ2h0PSIxODAiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSI1OS41NDY4NzUiIHk9Ijk0LjUiPjE3MXgxODA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=';
 
-const ImageLoading = ({ item, onCancelUpload }) => (
+const ImageLoading = ({ item, width, height, onCancelUpload }) => (
   <div className="zfe-files-ajax-preview-image thumbnail">
     <div className="btn-toolbar" role="toolbar">
       <Button icon="remove" title="Отменить" onClick={() => onCancelUpload(item.key)} />
     </div>
     <ProgressCircle percent={item.uploadProgress} />
-    <img src={item.previewLocal || LoadingMock} alt="Загрузка..." />
+    <div
+      className="zfe-files-ajax-preview-image-canvas"
+      style={{
+        backgroundImage: `url(${item.previewLocal || LoadingMock})`,
+        width: `${width || 200}px`,
+        height: `${height || 200}px`,
+      }}
+    />
   </div>
 );
 
@@ -23,7 +30,20 @@ ImageLoading.propTypes = {
     previewLocal: PropTypes.string,
     uploadProgress: PropTypes.number,
   }).isRequired,
+  width: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]),
+  height: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string,
+  ]),
   onCancelUpload: PropTypes.func.isRequired,
+};
+
+ImageLoading.defaultProps = {
+  width: null,
+  height: null,
 };
 
 export default ImageLoading;
