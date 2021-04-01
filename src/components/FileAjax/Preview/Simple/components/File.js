f24b320ea8d2a42985885aa307976c120e1e24c7
Ilya Serdyuk <ilya@serdyuk.pro>
2021-04-01 20:41:34 +0300

    Добавил статус загрузки «Обработка» – Сборка из чанков больших файлов может занимать продолжительное время и не очень понятно почему «залипает» на 100% – данное исправление улучшает ситуацию

--- src/components/FileAjax/Preview/Simple/components/File.js
+++ src/components/FileAjax/Preview/Simple/components/File.js
@@ -6,10 +6,12 @@ import Title from './Title';
 import DownloadLink from './DownloadLink';
 import Button from './Button';
 
+const workingMessage = (percentage) => (percentage < 100) ? `Загрузка… ${percentage ? `${percentage}%` : ''}` : 'Обработка…';
+
 const File = ({ item, onDelete, onUndelete }) => (
   <li className={item.deleted && 'deleted'}>
     <Icon />
-    <Title value={item.loading ? `Загрузка… ${item.uploadProgress ? `${Math.round(item.uploadProgress * 100) / 100}%` : ''}` : item.name} />
+    <Title value={item.loading ? workingMessage(item.uploadProgress ? Math.round(item.uploadProgress * 100) / 100 : null) : item.name} />
     {item.downloadUrl && <DownloadLink downloadUrl={item.downloadUrl} />}
     {item.deleted
       ? <Button icon="repeat" title="Восстановить" onClick={() => onUndelete(item.key)} className="undelete" />
