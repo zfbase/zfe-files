b97435cff27cbd77d272e17bdd4799b3bc9d7fad
Ilya Serdyuk <ilya@serdyuk.pro>
2023-03-06 12:32:38 +0300

    Добавил компоненту FileAjax/Preview/Simple/DownloadLink возможность добавлять классы CSS

--- src/components/FileAjax/Preview/Simple/components/DownloadLink.jsx
+++ src/components/FileAjax/Preview/Simple/components/DownloadLink.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DownloadLink = ({ downloadUrl }) => (
-  <a href={downloadUrl} target="_blank" title="Скачать оригинал">
+const DownloadLink = ({ downloadUrl, className }) => (
+  <a
+    href={downloadUrl}
+    target="_blank"
+    title="Скачать оригинал"
+    className={className}
+  >
     <span className="glyphicon glyphicon-download-alt" />
   </a>
 );
 
 DownloadLink.propTypes = {
   downloadUrl: PropTypes.string.isRequired,
+  className: PropTypes.string,
+};
+
+DownloadLink.defaultProps = {
+  className: undefined,
 };
 
 export default DownloadLink;
